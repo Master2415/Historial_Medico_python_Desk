6e6d99db55a5447e553898a3446792dff022f9da
master2415 <duutabarquino2415@gmail.com>
2024-06-05 21:05:39 -0500

    ventana calendario finish

--- Paciente/gui.py
+++ Paciente/gui.py
@@ -5,7 +5,6 @@ from tkinter import messagebox
 from tkinter import ttk, Toplevel
 from Model.pacienteDAO import *
 from tkcalendar import *
-from tkcalendar import Calendar
 from datetime import datetime, date
 
 
@@ -61,51 +60,60 @@ class Frame(tk.Frame):
         self.lblTelefono.config(font=('ARIAl',15,'bold'), bg='#CDD8FF')
         self.lblTelefono.grid(column=0,row=8, padx=10, pady=5)   
 
+        #LABEL BUSCADOR
+        self.lblBuscarDni = tk.Label(self, text='Buscar DNI: ')
+        self.lblBuscarDni.config(font=('ARIAl',15,'bold'), bg='#CDD8FF')
+        self.lblBuscarDni.grid(column=3, row=0, padx=10, pady=5)
+
+        self.lblBuscarApellido = tk.Label(self, text='Buscar Apellido: ')
+        self.lblBuscarApellido.config(font=('ARIAl',15,'bold'), bg='#CDD8FF')
+        self.lblBuscarApellido.grid(column=3, row=1, padx=10, pady=5)
+
     def entrys(self):
 
         self.svNombre = tk.StringVar()
         self.entryNombre = tk.Entry(self, textvariable=self.svNombre)
-        self.entryNombre.config(width=50, font=('ARIAL',15))
+        self.entryNombre.config(width=35, font=('ARIAL',15))
         self.entryNombre.grid(column=1, row=0, padx=10, pady=5, columnspan=2)
 
         self.svApePaterno = tk.StringVar()
         self.entryApePaterno = tk.Entry(self, textvariable=self.svApePaterno)
-        self.entryApePaterno.config(width=50, font=('ARIAL',15))
+        self.entryApePaterno.config(width=35, font=('ARIAL',15))
         self.entryApePaterno.grid(column=1, row=1, padx=10, pady=5, columnspan=2)
 
         self.svApeMaterno = tk.StringVar()
         self.entryApeMaterno = tk.Entry(self, textvariable=self.svApeMaterno)
-        self.entryApeMaterno.config(width=50, font=('ARIAL',15))
+        self.entryApeMaterno.config(width=35, font=('ARIAL',15))
         self.entryApeMaterno.grid(column=1, row=2, padx=10, pady=5, columnspan=2)
 
         self.svDni = tk.StringVar()
         self.entryDni = tk.Entry(self, textvariable=self.svDni)
-        self.entryDni.config(width=50, font=('ARIAL',15))
+        self.entryDni.config(width=35, font=('ARIAL',15))
         self.entryDni.grid(column=1, row=3, padx=10, pady=5, columnspan=2)
 
         self.svFecNacimiento = tk.StringVar()
         self.entryFecNacimiento = tk.Entry(self, textvariable=self.svFecNacimiento)
-        self.entryFecNacimiento.config(width=50, font=('ARIAL',15))
+        self.entryFecNacimiento.config(width=35, font=('ARIAL',15))
         self.entryFecNacimiento.grid(column=1, row=4, padx=10, pady=5, columnspan=2)
 
         self.svEdad = tk.StringVar()
         self.entryEdad = tk.Entry(self, textvariable=self.svEdad)
-        self.entryEdad.config(width=50, font=('ARIAL',15))
+        self.entryEdad.config(width=35, font=('ARIAL',15))
         self.entryEdad.grid(column=1, row=5, padx=10, pady=5, columnspan=2)
 
         self.svAntecentes = tk.StringVar()
         self.entryAntecedentes = tk.Entry(self, textvariable=self.svAntecentes)
-        self.entryAntecedentes.config(width=50, font=('ARIAL',15))
+        self.entryAntecedentes.config(width=35, font=('ARIAL',15))
         self.entryAntecedentes.grid(column=1, row=6, padx=10, pady=5, columnspan=2)
 
         self.svCorreo = tk.StringVar()
         self.entryCorreo = tk.Entry(self, textvariable=self.svCorreo)
-        self.entryCorreo.config(width=50, font=('ARIAL',15))
+        self.entryCorreo.config(width=35, font=('ARIAL',15))
         self.entryCorreo.grid(column=1, row=7, padx=10, pady=5, columnspan=2)
 
         self.svTelefono = tk.StringVar()
         self.entryTelefono = tk.Entry(self, textvariable=self.svTelefono)
-        self.entryTelefono.config(width=50, font=('ARIAL',15))
+        self.entryTelefono.config(width=35, font=('ARIAL',15))
         self.entryTelefono.grid(column=1, row=8, padx=10, pady=5, columnspan=2)
 
         #ENTRYS BUSCADOR
@@ -127,20 +135,11 @@ class Frame(tk.Frame):
 
         self.btnGuardar = tk.Button(self, text='Guardar', command=self.guardarPaciente)
         self.btnGuardar.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#000000', cursor='hand2',activebackground='#5F5F5F')
-        self.btnGuardar.grid(column=1, row=9, padx=10, pady=5)
+        self.btnGuardar.grid(column=2, row=9, padx=10, pady=5)
 
         self.btnCancelar = tk.Button(self, text='Cancelar', command=self.deshabilitar_Entrys)
         self.btnCancelar.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#B00000', cursor='hand2',activebackground='#D27C7C')
-        self.btnCancelar.grid(column=2, row=9, padx=10, pady=5) 
-
-        #LABEL BUSCADOR
-        self.lblBuscarDni = tk.Label(self, text='Buscar DNI: ')
-        self.lblBuscarDni.config(font=('ARIAl',15,'bold'), bg='#CDD8FF')
-        self.lblBuscarDni.grid(column=3, row=0, padx=10, pady=5)
-
-        self.lblBuscarApellido = tk.Label(self, text='Buscar Apellido: ')
-        self.lblBuscarApellido.config(font=('ARIAl',15,'bold'), bg='#CDD8FF')
-        self.lblBuscarApellido.grid(column=3, row=1, padx=10, pady=5)
+        self.btnCancelar.grid(column=4, row=9, padx=10, pady=5) 
 
         #BUTTON BUSCADOR
         self.btnBuscarCondicion = tk.Button(self, text='Buscar', command = self.buscarCondicion)
@@ -152,11 +151,11 @@ class Frame(tk.Frame):
         self.btnLimpiarBuscador.grid(column=4,row=2, padx=10, pady=5, columnspan=1)
 
         #BUTTON DE LA TABLA
-        self.btnEditarPaciente = tk.Button(self, text='Editar Paciente', command=self.editarPaciente)
+        self.btnEditarPaciente = tk.Button(self, text='Editar', command=self.editarPaciente)
         self.btnEditarPaciente.config(width=20,font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#1E0075', activebackground='#9379E0', cursor='hand2')
         self.btnEditarPaciente.grid(row=11, column=0, padx=10, pady=5)
 
-        self.btnEliminarPaciente = tk.Button(self, text='Eliminar Paciente', command=self.eliminarDatoPaciente )
+        self.btnEliminarPaciente = tk.Button(self, text='Eliminar', command=self.eliminarDatoPaciente )
         self.btnEliminarPaciente.config(width=20,font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#8A0000', activebackground='#D58A8A', cursor='hand2')
         self.btnEliminarPaciente.grid(row=11, column=1, padx=10, pady=5)
 
@@ -170,19 +169,59 @@ class Frame(tk.Frame):
 
         self.btnCalendario = tk.Button(self, text='Calendario', command = self.vistaCalendario)
         self.btnCalendario.config(width=20, font=('ARIAL',12,'bold'), fg='#DAD5D6', bg='#53005B', cursor='hand2',activebackground='#C774CF')
-        self.btnCalendario.grid(column=3,row=3, padx=10, pady=5, columnspan=1)
+        self.btnCalendario.grid(column=1, row=9, padx=10, pady=5)
 
     def vistaCalendario(self):
         self.topCalendario = Toplevel()
-        self.topCalendario.title("FECHA NACIMIENTO")
+        self.topCalendario.title("FECHA")
         self.topCalendario.resizable(0,0)
         #self.topCalendario.iconbitmap('img/clinica.ico')
         self.topCalendario.config(bg='#CDD8FF')
 
-        self.svCalendario = StringVar()
-        self.calendar = tc.Calendar(self.topCalendario, selectmode='day', year=2015, month=1, day=1)
+        # Se obtiene la fecha actual del sistema
+        fecha_actual = date.today()
+        # Se crea una cadena de texto con la fecha actual en el formato deseado (dd-mm-YYYY)
+        fecha_actual_str = fecha_actual.strftime("%d-%m-%Y")
+        # Se crea una variable de control para el calendario y se establece la fecha actual como valor inicial
+        self.svCalendario = StringVar(value=fecha_actual_str)
+
+        # Se crea el widget de calendario, se asocia con la variable de control y se empaqueta en la ventana superior
+        self.calendar = tc.Calendar(
+            self.topCalendario,
+            selectmode='day',
+            year=2015,
+            month=1,
+            day=1,
+            bg='#777777',
+            fg='#FFFFFF',
+            headersbackground='#B6DDFE',
+            cursor='hand2',
+            date_pattern='dd-mm-Y',
+            textvariable=self.svCalendario
+        )
         self.calendar.pack(pady=22)
-        self.calendar.grid(row=1, column = 0)
+
+        #TRACE ENVIAR FECHA
+        self.svCalendario.trace('w',self.enviarFecha)
+
+    def enviarFecha(self, *args):
+        self.svFecNacimiento.set('' + self.svCalendario.get())
+        if len(self.calendar.get_date()) > 1:
+            self.svCalendario.trace('w', self.calcularEdad)
+
+    def calcularEdad(self, *args):
+        # Obtiene la fecha actual del sistema
+        self.fechaActual = date.today()
+        # Obtiene la fecha seleccionada del widget de calendario
+        self.date1 = self.calendar.get_date()
+        # Convierte la fecha de cadena a un objeto datetime
+        self.conver = datetime.strptime(self.date1, "%d-%m-%Y")
+        # Calcula la diferencia en años entre la fecha actual y la fecha de nacimiento
+        self.resul = self.fechaActual.year - self.conver.year
+        # Ajusta la diferencia en años si aún no ha pasado el cumpleaños de la persona este año
+        self.resul -= ((self.fechaActual.month, self.fechaActual.day) < (self.conver.month, self.conver.day))
+        # Establece el valor de una variable de control con la edad calculada
+        self.svEdad.set(self.resul)
 
     def guardarPaciente(self):
         persona = Persona(
@@ -212,7 +251,6 @@ class Frame(tk.Frame):
         else:
             self.tablaPaciente()
 
-
     def deshabilitar_Entrys(self):  
         self.idPersona = None
         self.svNombre.set('')
@@ -239,6 +277,7 @@ class Frame(tk.Frame):
         # Deshabilito los botones
         self.btnGuardar.config(state='disabled')
         self.btnCancelar.config(state='disabled')
+        self.btnCalendario.config(state='disabled')
 
     def habilitar(self):
         self.svNombre.set('')
@@ -263,7 +302,8 @@ class Frame(tk.Frame):
 
         self.btnGuardar.config(state='normal')
         self.btnCancelar.config(state='normal') 
-
+        self.btnCalendario.config(state='normal')
+    
     def editarPaciente(self):
         try:
             # Obtengo los datos
@@ -321,13 +361,13 @@ class Frame(tk.Frame):
     
 
         self.tabla = ttk.Treeview(self, column=('Nombre', 'APaterno', 'AMaterno','Dni','FNacimiento','Edad','Antecedentes','Correo','Telefono'))
-        self.tabla.grid(column=0, row=10, columnspan=10, sticky='nse')  
+        self.tabla.grid(column=0, row=10, columnspan=10, sticky='nsew')  
         
         self.scroll = ttk.Scrollbar(self, orient='vertical', command=self.tabla.yview)
-        self.scroll.grid(row=10, column=11, sticky='nse')
+        self.scroll.grid(column=11, row=10, sticky='nse')
 
         self.tabla.configure(yscrollcommand=self.scroll.set)
-        self.tabla.tag_configure('evenrow', background='#C5EAFE')
+        self.tabla.tag_configure('evenrow', background='#D7D7D7')
 
         self.tabla.heading('#0',text='ID')
         self.tabla.heading('#1',text='Nombre')
